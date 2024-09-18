5014e1d7082a3e046e86dbbf47151eca34b3ab64
JLeo-83 <143038770+JLeo-83@users.noreply.github.com>
2024-09-18 18:44:35 -0500

    Word and Clients

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 
 import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
-import About from "./components/About";
+import About from "./sections/About";
+import Projects from "./sections/Projects";
+import Clients from "./sections/Clients";
 const App = () => {
   return (
     <main className="max-w-7xl mx-auto">
       <Navbar />
       <Hero/>  
       <About/>
+      <Projects/>
+      <Clients/>
     </main>
   )
 } 
--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,28 +1,28 @@
 import Globe from "react-globe.gl"
 import Button from "../components/Button"
 import { useState } from "react";
-    const About = () => {
-        
-        const [hasCopied, setHasCopied] = useState(false);
+const About = () => {
 
-        const handleCopy = () => {
-            navigator.clipboard.writeText("leandro@jsjunior.pro");
-            setHasCopied(true);
-            setTimeout(() => {
-                setHasCopied(false);
-            }, 2000);
-        }
+    const [hasCopied, setHasCopied] = useState(false);
 
-      return (
-        <section className="c-space my-20">
+    const handleCopy = () => {
+        navigator.clipboard.writeText("leandro@jsjunior.pro");
+        setHasCopied(true);
+        setTimeout(() => {
+            setHasCopied(false);
+        }, 2000);
+    }
+
+    return (
+        <section className="c-space my-20" id="about">
             <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
                 <div className="col-span-1 xl:row-span-3">
                     <div className="grid-container">
-                        <img src="/assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain"/>
+                        <img src="/assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain" />
 
                         <div>
                             <p className="grid-headtext">
-                               Hi, I'm Leandro 
+                                Hi, I'm Leandro
                             </p>
                             <p className="grid-subtext">
                                 With 12 years of experience, Ihave honed my skill in front-end and back-end development, with a focus on animated 3D websites.
@@ -43,27 +43,27 @@ import { useState } from "react";
                 <div className="col-span-1 xl:row-span-4">
                     <div className="grid-container">
                         <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
-                            <Globe 
-                            height={326}
-                            width={326}
-                        backgroundColor="rgba(0, 0, 0, 0)"
-                        backgroundImageOpacity={0.5}
-                        showAtmosphere
-                        showGraticules
-                        globeImageUrl="//unpkg.com/three-globe/example/img/earth-day.jpg"
-                        bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+                            <Globe
+                                height={326}
+                                width={326}
+                                backgroundColor="rgba(0, 0, 0, 0)"
+                                backgroundImageOpacity={0.5}
+                                showAtmosphere
+                                showGraticules
+                                globeImageUrl="//unpkg.com/three-globe/example/img/earth-day.jpg"
+                                bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
                             />
                         </div>
                         <div>
                             <p className="grid-headtext">I work remotely across most timezones.</p>
                             <p className="grid-subtext">I'm basic in Canada, with work available.</p>
-                            <Button name="Contact Me" isBeam containerClass="w-full mt-10"/>
+                            <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
                         </div>
                     </div>
                 </div>
                 <div className="xl:col-span-2 xl:row-span-3">
                     <div className="grid-container">
-                        <img src="/assets/grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain"/>
+                        <img src="/assets/grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain" />
                         <div>
                             <p className="grid-headtext">My Passion for Coding</p>
                             <p className="grid-subtext">I love solving problems and building things through code. Coding isn't just my profession - it is my passion.</p>
@@ -77,15 +77,15 @@ import { useState } from "react";
                         <div className="space-y-2">
                             <p className="grid-subtext text-center">Contact me</p>
                             <div className="copy-container" onClick={handleCopy}>
-                            <img src={hasCopied ? 'assets/tick.svg' : 'assets/copy.svg'}alt="copy" />
-                            <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">leandro@jsjunior.pro</p>
+                                <img src={hasCopied ? 'assets/tick.svg' : 'assets/copy.svg'} alt="copy" />
+                                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">leandro@jsjunior.pro</p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
-      )
-    }
-    
-    export default About
+    )
+}
+
+export default About
